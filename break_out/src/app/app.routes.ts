14e037311d7534801ff4d910f84adc10a41261d8
TriGonostylops <martonbalazsi@gmail.com>
2025-04-10 13:12:14 +0200

    feat: simple navbar

--- break_out/src/app/app.routes.ts
+++ break_out/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./pages/home/home.component').then((m) => m.HomeComponent);
+    },
+  },
+  {
+    path: 'register',
+    loadComponent: () => {
+        return import('./pages/register/register.component').then((m) => m.RegisterComponent);
+    },
+  },
+  {
+    path: 'login',
+    loadComponent: () => {
+        return import('./pages/login/login.component').then((m) => m.LoginComponent);
+    },
+  },
+];
