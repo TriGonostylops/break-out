090bb29b353c1be4e2f91f45a3fbc7af7469ae1c
TriGonostylops <martonbalazsi@gmail.com>
2025-05-18 12:35:32 +0200

    feat: basic side-nav

--- break_out/src/app/components/header/header.component.ts
+++ break_out/src/app/components/header/header.component.ts
@@ -1,26 +1,62 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef,
+  OnInit,
+  NgZone,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { auth } from '../../../firebase/firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { NgIf } from '@angular/common';
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, NgIf],
+  imports: [RouterLink, NgIf, MatSidenavModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit {
   @Input() title!: string;
-
   isLoggedIn: boolean = false;
+  isMobile: boolean = false;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private cdr: ChangeDetectorRef,
+    private ngZone: NgZone
+  ) {}
+
   ngOnInit(): void {
     onAuthStateChanged(auth, (user) => {
-      this.isLoggedIn = !!user; 
+      this.ngZone.run(() => {
+        this.isLoggedIn = !!user;
+        this.cdr.detectChanges();
+      });
     });
+
+    this.breakpointObserver
+      .observe([Breakpoints.Handset, '(max-width: 768px)'])
+      .subscribe((result) => {
+        this.ngZone.run(() => {
+          this.isMobile = result.matches;
+          this.cdr.detectChanges();
+          if (!this.isMobile && this.sidenav) {
+            this.sidenav.close();
+          }
+        });
+      });
   }
+
+  ngAfterViewInit(): void {
+  }
+
   async logout(): Promise<void> {
     try {
       await signOut(auth);
